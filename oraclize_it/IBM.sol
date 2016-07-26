300db04b214d1fa2e5d44792c0f1860b37538376
Harry Pottash <giblfiz@gmail.com>
2016-07-25 17:32:32 -0700

    added updated_time to oracles

--- oraclize_it/IBM.sol
+++ oraclize_it/IBM.sol
@@ -1,28 +1,26 @@
 // Ethereum + Solidity
-// This pulls financial data from Yahoo
-// and pushes it onto the blockchain
-
+// This code sample & more @ dev.oraclize.it
 
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
-contract IBMUSDfeed is usingOraclize {
-  uint public IBM_USD;
+contract IBM_USDfeed is usingOraclize {
+  uint public USD;
+  uint public updated_time;
     
-  function IBMUSDfeed(){
+  function IBM_USDfeed(){
     oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
     update(0); // first check at contract creation
   }
     
   function __callback(bytes32 myid, string result, bytes proof) {
     if (msg.sender != oraclize_cbAddress()) throw;
-    IBM_USD = parseInt(result, 2); // save it as $ cents
-    // do something with ETHUSD
-    update(3600); // schedule another check in hour
+    USD = parseInt(result, 2); // save it as $ cents
+    updated_time = block.timestamp;
   }
   
   function update(uint delay){
     // call oraclize and retrieve the latest IBM/USD price from Yahoo's YQL API
-    // YQL == select LastTradePriceOnly from yahoo.finance.quote where symbol = "IBM"
+    // YQL == select LastTradePriceOnly from yahoo.finance.quote where symbol IBM 
     oraclize_query(delay, "URL",
       "json(https://query.yahooapis.com/v1/public/yql?q=select%20LastTradePriceOnly%20from%20yahoo.finance.quote%20where%20symbol%20%3D%20%22IBM%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys).query.results.quote.LastTradePriceOnly");
   }
