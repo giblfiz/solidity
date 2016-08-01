ad956360924f8f6d4702a0a054fd50138873dd8b
Harry Pottash <giblfiz@gmail.com>
2016-08-01 15:01:24 -0700

    Update stdOracle.sol

--- libs/stdOracle.sol
+++ libs/stdOracle.sol
@@ -1,1 +1,16 @@
 import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+
+
+contract stdOracle is usingOraclize {
+  uint public value; //The value of the oraclized thing, usually a price in usd
+  uint public updated_time; //When was the value set?
+  uint public priceToUse; //how many Wei for us to take your contract 
+  string public name; //name within the MVSC schema Probably stock ticker
+
+  string query; //this is the call out, at the moment mostly to YUI
+  address[] requestors; //list of users to be notified on update
+
+  address owner; //who has update perms? 
+
+}
