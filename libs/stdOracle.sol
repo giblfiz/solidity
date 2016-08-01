8af0f31b87d2019aaab8d457dc5b205830a60c04
Harry Pottash <giblfiz@gmail.com>
2016-08-01 15:02:24 -0700

    Update stdOracle.sol

--- libs/stdOracle.sol
+++ libs/stdOracle.sol
@@ -13,4 +13,44 @@ contract stdOracle is usingOraclize {
 
   address owner; //who has update perms? 
 
+
+  function stdOracle(string _name){
+    name = _name; //something like "IBM", "AAPL", or "FB"
+    owner = msg.sender;
+    priceToUse = 10 finney;
+    oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
+    
+    // YQL == select LastTradePriceOnly from yahoo.finance.quote where symbol IBM 
+    var start = "json(https://query.yahooapis.com/v1/public/yql?q=select%20LastTradePriceOnly%20from%20yahoo.finance.quote%20where%20symbol%20%3D%20%22";
+    var end = "%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys).query.results.quote.LastTradePriceOnly";
+    query = strConcat(start,name,end);
+    
+    //todo: put auto-registration in place... something like
+    // registrar reg = registrar(someaddress)
+    // reg.register(name, this)
+    
+  }
+    
+  function __callback(bytes32 myid, string result, bytes proof) {
+    if (msg.sender != oraclize_cbAddress()) throw;
+    value = parseInt(result, 2); // save it as $ cents
+    updated_time = block.timestamp;
+    
+    for (uint i = 0; i < requestors.length; i++){
+        origen o = origen(requestors[i]);
+        o.__callback(name);
+    }
+    delete requestors;
+  }
+  
+  function update(){
+    if(msg.value < priceToUse ){
+        throw; // pay for use! 
+    }
+    // call oraclize and retrieve the latest price from Yahoo's YQL API
+    oraclize_query(0, "URL", query); //delay set to zero, Maybe play with spare gas options later.
+    requestors.push(msg.sender);    
+  }
+  
+
 }
