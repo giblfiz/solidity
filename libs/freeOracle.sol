82c4a35f63f7add5f910dd003623e0e751867d33
Harry Pottash <giblfiz@gmail.com>
2016-08-09 19:07:33 -0700

    Update and rename free_oracle.sol to freeOracle.sol

--- libs/freeOracle.sol
+++ libs/freeOracle.sol
@@ -1,12 +1,12 @@
 // There is a copy of this named eth2us¢ at 0xcf631a0c28b9ebcf4a94296e2f7ff050e1954f56 on testnet
 
-contract free_oracle {
+contract freeOracle {
     uint public value; //The value of the oraclized thing, usually a price in usd
     uint public updated_time; //When was the value set?
     string public name; //name within the MVSC schema Probably stock ticker
     address owner; // who can make a change;
 
-    function free_oracle(string _name){
+    function freeOracle(string _name){
         name = _name;
         owner = msg.sender;
     }
@@ -19,7 +19,7 @@ contract free_oracle {
         updated_time = block.timestamp;
     }
 
-    function set_owner(address _owner) returns (string) {
+    function setOwner(address _owner) returns (string) {
         if(msg.sender != owner){
             throw;
         }
