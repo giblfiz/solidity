90db0ee1fcb61d81c6f150c2ed6e2900f29d2172
Harry Pottash <giblfiz@gmail.com>
2016-08-15 11:09:06 -0700

    Update mvscOracle.sol
    So... added ability to collect multiple requests, and have them all serviced.
    
    Two things that need to happen:
      Gas management (backend issue)
      request identifier in the callback, so if you have multiple calls to the oracle, you can sort out what you get back.

--- libs/mvscOracle.sol
+++ libs/mvscOracle.sol
@@ -1,43 +1,56 @@
+//Version 0.3
+
 import "github.com/giblfiz/solidity/libs/owned.sol";
 
-
 contract callerStub{
-    function __callback(){
+    function __callback(uint value, string target, string error){
     }
 }
 
 contract mvscOracle is owned{
-    string public name;
-    uint public value;
+    struct req{
+        address callbackTo;
+        uint gasValue;
+        string stock;
+    }
     
-    address callbackTo;
-    uint gasValue;
+    req[] reqs;
     
-    function mvscOracle(string _n){
+    function mvscOracle(){
         owner = msg.sender;
-        name = _n;
     }
     
-    function request(){
-        callbackTo = msg.sender;
-        gasValue = msg.value;
+    function request(string _stock){
+        var r = req({callbackTo:msg.sender,gasValue:msg.value, stock:_stock});
+        reqs.push(r);
     }
 
-    function getCaller() returns (address){
+    function getCaller(uint _n) returns (address){
         checkOwnership();
-        return callbackTo;
+        if(reqs.length < _n){
+            return 0x0;
+        }
+        return reqs[_n].callbackTo;
     }
     
-    function getGasValue() returns (uint){
+    function getGasValue(uint _n) returns (uint){
         checkOwnership();
-        return gasValue;
+        return reqs[_n].gasValue;
     }
 
-    function update(uint _v){
+    function getName(uint _n) returns (string){
         checkOwnership();
-        value = _v;
-        var caller = callerStub(callbackTo);
-        caller.__callback();
-        callbackTo = 0x0;
+        return reqs[_n].stock;
+    }
+
+    function clearReqs(){
+        checkOwnership();
+        delete reqs;
+    }
+
+    function update(address _a, uint _v, string _t, string _e){
+        checkOwnership();
+        var caller = callerStub(_a);
+        caller.__callback(_v, _t, _e);
     }    
 }
